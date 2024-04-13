a6a2db49f06f2c23c37563375bc5be7b8570b234
deoism <deofunds@gmail.com>
2024-04-12 23:50:21 -0400

    added final resport

--- imageManipulationCV.py
+++ imageManipulationCV.py
@@ -1,7 +1,8 @@
-import requests
-import matplotlib.pyplot as plt
+import os
 import cv2
+import requests
 import numpy as np
+import matplotlib.pyplot as plt
 
 urls = [
     "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-CV0101EN-SkillsNetwork/images%20/images_part_1/cat.png",
@@ -15,29 +16,23 @@ for url in urls:
     with open(filename, 'wb') as f:
         f.write(response.content)
 
-
-#copying images
-
 baboon = cv2.imread("baboon.png")
 plt.figure(figsize=(10,10))
 plt.imshow(cv2.cvtColor(baboon, cv2.COLOR_BGR2RGB))
 plt.show()
 
-#create and compare copy to assignment
 A = baboon
 
-id(A)==id(baboon)
-id(A)
+id_A = id(A)
+print(id_A)
 
 B = baboon.copy()
-id(B)==id(baboon)
-
-#When we do not apply the method copy(), the variable will point to the same location in memory. Consider the array baboon, if we set all its values to zero, then all the values in A will be zero. This is because baboon and A point to the same place in memory, but B will not be affected.
+id_B = id(B)
+print(id_B)
 
 baboon[:,:,] = 0
 
 plt.figure(figsize=(10,10))
-
 plt.subplot(121)
 plt.imshow(cv2.cvtColor(baboon, cv2.COLOR_BGR2RGB))
 plt.title("baboon")
@@ -45,7 +40,6 @@ plt.title("baboon")
 plt.subplot(122)
 plt.imshow(cv2.cvtColor(A, cv2.COLOR_BGR2RGB))
 plt.title("array A")
-
 plt.show()
 
 plt.figure(figsize=(10,10))
@@ -57,8 +51,8 @@ plt.imshow(cv2.cvtColor(B, cv2.COLOR_BGR2RGB))
 plt.title("array B")
 plt.show()
 
+plt.close()
 
-#FLIPPING IMAGES
 print('Flipping images')
 
 image = cv2.imread("cat.png")
@@ -66,101 +60,87 @@ plt.figure(figsize=(10,10))
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.title("Original Image")
 plt.show()
+plt.close()
 
-#cast to array to find the shape
-width, height,C=image.shape
-print('width, height,C',width, height,C)
+width, height, C = image.shape
+print('width, height, C', width, height, C)
 
-#Flip i.e rotate it vertically. First, we create an array of equal size of type np.uint8 bit image
- 
-array_flip = np.zeros((width, height,C),dtype=np.uint8)
+array_flip = np.zeros((width, height, C), dtype=np.uint8)
 
-#We assign the first row of pixels of the original array to the new array's last row. We repeat the process for every row, incrementing the row number for the original array and decreasing the new array's row index assigning the pixels accordingly. After executing the for loop below, array_flip will become the flipped image.
+for i, row in enumerate(image):
+    array_flip[width-1-i, :, :] = row
+    print("flipcode : ", i, "row: ", row)
 
-
-#flipcode = 0: flip vertically around the x-axis
-#flipcode > 0: flip horizontally around y-axis positive value
-#flipcode< 0: flip vertically and horizontally, flipping around both axes negative value
-for i,row in enumerate(image):
-        array_flip[width-1-i,:,:]=row
-
-plt.figure(figsize=(5,5))
-plt.imshow(cv2.cvtColor(array_flip, cv2.COLOR_BGR2RGB))
-plt.title(f"flipcode: ", i)
+    plt.figure(figsize=(5, 5))
+    plt.imshow(cv2.cvtColor(array_flip, cv2.COLOR_BGR2RGB))
+    plt.title("flipcode:")
 plt.show()
+plt.close()
 
-
-#flipcode in a loop
-for flipcode in [0,1,-1]:
-    im_flip =  cv2.flip(image,flipcode )
-    plt.imshow(cv2.cvtColor(im_flip,cv2.COLOR_BGR2RGB))
+for flipcode in [0, 1, -1]:
+    im_flip =  cv2.flip(image, flipcode)
+    plt.imshow(cv2.cvtColor(im_flip, cv2.COLOR_BGR2RGB))
     plt.title("flipcode: "+str(flipcode))
     plt.show()
+    plt.close()
 
-im_flip = cv2.rotate(image,0)
-plt.imshow(cv2.cvtColor(im_flip,cv2.COLOR_BGR2RGB))
+im_flip = cv2.rotate(image, 0)
+plt.imshow(cv2.cvtColor(im_flip, cv2.COLOR_BGR2RGB))
 plt.title("rotate: 0")
 plt.show()
+plt.close()
 
-#OpenCV module has built-in attributes the describe the type of flip, the values are just integers. Several are shown in the following dict:
+flip = {"ROTATE_90_CLOCKWISE": cv2.ROTATE_90_CLOCKWISE, "ROTATE_90_COUNTERCLOCKWISE": cv2.ROTATE_90_COUNTERCLOCKWISE, "ROTATE_180": cv2.ROTATE_180}
 
-flip = {"ROTATE_90_CLOCKWISE":cv2.ROTATE_90_CLOCKWISE,"ROTATE_90_COUNTERCLOCKWISE":cv2.ROTATE_90_COUNTERCLOCKWISE,"ROTATE_180":cv2.ROTATE_180}
-
-print("flip rotate 90 clockwize : ",flip["ROTATE_90_CLOCKWISE"])
+print("flip rotate 90 clockwise: ", flip["ROTATE_90_CLOCKWISE"])
 
 for key, value in flip.items():
-    plt.subplot(1,2,1)
+    plt.subplot(1, 2, 1)
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.title("orignal")
-    plt.subplot(1,2,2)
-    plt.imshow(cv2.cvtColor(cv2.rotate(image,value), cv2.COLOR_BGR2RGB))
+    plt.title("original")
+    plt.subplot(1, 2, 2)
+    plt.imshow(cv2.cvtColor(cv2.rotate(image, value), cv2.COLOR_BGR2RGB))
     plt.title(key)
     plt.show()
+    plt.close()
 
-#cropping images
 print("Cropping images")
 
-upper = 150
-lower = 400
-crop_top = image[upper: lower,:,:]
-plt.figure(figsize=(10,10))
+UPPER = 150
+LOWER = 400
+crop_top = image[UPPER:LOWER, :, :]
+plt.figure(figsize=(10, 10))
 plt.imshow(cv2.cvtColor(crop_top, cv2.COLOR_BGR2RGB))
 plt.show()
 
-#croptop
-left = 150
-right = 400
-crop_horizontal = crop_top[: ,left:right,:]
-plt.figure(figsize=(5,5))
+LEFT = 150
+RIGHT = 400
+crop_horizontal = crop_top[:, LEFT:RIGHT, :]
+plt.figure(figsize=(5, 5))
 plt.imshow(cv2.cvtColor(crop_horizontal, cv2.COLOR_BGR2RGB))
 plt.show()
 
-
-#changing specific image pixels
-
 array_sq = np.copy(image)
-array_sq[upper:lower,left:right,:] = 0
+array_sq[UPPER:LOWER, LEFT:RIGHT, :] = 0
 
-#comparing results to new image
-plt.figure(figsize=(10,10))
-plt.subplot(1,2,1)
-plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
-plt.title("orignal")
-plt.subplot(1,2,2)
-plt.imshow(cv2.cvtColor(array_sq,cv2.COLOR_BGR2RGB))
+plt.figure(figsize=(10, 10))
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title("original")
+plt.subplot(1, 2, 2)
+plt.imshow(cv2.cvtColor(array_sq, cv2.COLOR_BGR2RGB))
 plt.title("Altered Image")
 plt.show()
 
-start_point, end_point = (left, upper),(right, lower)
+start_point, end_point = (LEFT, UPPER), (RIGHT, LOWER)
 image_draw = np.copy(image)
-cv2.rectangle(image_draw, pt1=start_point, pt2=end_point, color=(0, 255, 0), thickness=3) 
-plt.figure(figsize=(5,5))
+cv2.rectangle(image_draw, pt1=start_point, pt2=end_point, color=(0, 255, 0), thickness=3)
+plt.figure(figsize=(5, 5))
 plt.imshow(cv2.cvtColor(image_draw, cv2.COLOR_BGR2RGB))
 plt.show()
 
-#overlay text
 print("Overlay Text")
-image_draw=cv2.putText(img=image,text='Stuff',org=(10,500),color=(255,255,255),fontFace=4,fontScale=5,thickness=2)
-plt.figure(figsize=(10,10))
-plt.imshow(cv2.cvtColor(image_draw,cv2.COLOR_BGR2RGB))
+image_draw = cv2.putText(img=image, text='Stuff', org=(10, 500), color=(255, 255, 255), fontFace=4, fontScale=5, thickness=2)
+plt.figure(figsize=(10, 10))    
+plt.imshow(cv2.cvtColor(image_draw, cv2.COLOR_BGR2RGB))
 plt.show()
