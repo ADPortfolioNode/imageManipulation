292fa398cf0ad137d2d975171745caab1f3a7446
deoism <deofunds@gmail.com>
2024-04-14 11:10:39 -0400

    Refactor image manipulation code and add image flipping and mirroring functionality
    Signed-off-by: deoism <deofunds@gmail.com>

--- imageManipulationCV.py
+++ imageManipulationCV.py
@@ -1,4 +1,5 @@
 import os
+import string
 import cv2
 import requests
 import numpy as np
@@ -17,7 +18,7 @@ for url in urls:
         f.write(response.content)
 
 baboon = cv2.imread("baboon.png")
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(3,5))
 plt.imshow(cv2.cvtColor(baboon, cv2.COLOR_BGR2RGB))
 plt.show()
 
@@ -32,7 +33,7 @@ print(id_B)
 
 baboon[:,:,] = 0
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(5,5))
 plt.subplot(121)
 plt.imshow(cv2.cvtColor(baboon, cv2.COLOR_BGR2RGB))
 plt.title("baboon")
@@ -42,7 +43,7 @@ plt.imshow(cv2.cvtColor(A, cv2.COLOR_BGR2RGB))
 plt.title("array A")
 plt.show()
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(5,5))
 plt.subplot(121)
 plt.imshow(cv2.cvtColor(baboon, cv2.COLOR_BGR2RGB))
 plt.title("baboon")
@@ -50,45 +51,53 @@ plt.subplot(122)
 plt.imshow(cv2.cvtColor(B, cv2.COLOR_BGR2RGB))
 plt.title("array B")
 plt.show()
-
-plt.close()
+ 
 
 print('Flipping images')
 
 image = cv2.imread("cat.png")
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(5,5))
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.title("Original Image")
 plt.show()
 plt.close()
 
 width, height, C = image.shape
+import cv2 as cv
+
 print('width, height, C', width, height, C)
 
 array_flip = np.zeros((width, height, C), dtype=np.uint8)
 
 for i, row in enumerate(image):
     array_flip[width-1-i, :, :] = row
-    print("flipcode : ", i, "row: ", row)
+    print("flipcode : ", i, "row: ")
 
-    plt.figure(figsize=(5, 5))
-    plt.imshow(cv2.cvtColor(array_flip, cv2.COLOR_BGR2RGB))
-    plt.title("flipcode:")
-plt.show()
-plt.close()
+
+     
 
 for flipcode in [0, 1, -1]:
     im_flip =  cv2.flip(image, flipcode)
+    print("flipcode : ", flipcode)
+    plt.figure(figsize=(5,5))
     plt.imshow(cv2.cvtColor(im_flip, cv2.COLOR_BGR2RGB))
     plt.title("flipcode: "+str(flipcode))
     plt.show()
+    cv2.imwrite('flipcode'+str(flipcode)+'.png', im_flip)
+    print("flipcode : ", flipcode)
     plt.close()
 
-im_flip = cv2.rotate(image, 0)
-plt.imshow(cv2.cvtColor(im_flip, cv2.COLOR_BGR2RGB))
-plt.title("rotate: 0")
+print("Rotating images")
+image = cv2.imread("cat.png")
+plt.figure(figsize=(5,5))
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title("Original Image")
 plt.show()
 plt.close()
+
+
+im_flip = cv2.rotate(image, 0)
+cv2.imwrite('rotate_zero.png', im_flip)
 
 flip = {"ROTATE_90_CLOCKWISE": cv2.ROTATE_90_CLOCKWISE, "ROTATE_90_COUNTERCLOCKWISE": cv2.ROTATE_90_COUNTERCLOCKWISE, "ROTATE_180": cv2.ROTATE_180}
 
@@ -100,8 +109,10 @@ for key, value in flip.items():
     plt.title("original")
     plt.subplot(1, 2, 2)
     plt.imshow(cv2.cvtColor(cv2.rotate(image, value), cv2.COLOR_BGR2RGB))
-    plt.title(key)
+    plt.title(key) 
     plt.show()
+    # Save plt to image and save image
+    plt.savefig(key + ".png")
     plt.close()
 
 print("Cropping images")
@@ -144,3 +155,33 @@ image_draw = cv2.putText(img=image, text='Stuff', org=(10, 500), color=(255, 255
 plt.figure(figsize=(10, 10))    
 plt.imshow(cv2.cvtColor(image_draw, cv2.COLOR_BGR2RGB))
 plt.show()
+
+# write your code here
+im = cv2.imread("baboon.png")
+#convert from bgr to rgb
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+
+im_flip = cv2.flip(im, 0)
+im_mirror = cv2.flip(im, 1)
+
+#plot both images
+
+#flip
+plt.figure(figsize=(10,10))
+plt.subplot(1,2,1)
+plt.imshow(cv2.cvtColor(im,cv2.COLOR_BGR2RGB))
+plt.title("orignal")
+plt.subplot(1,2,2)
+plt.imshow(cv2.cvtColor(im_flip,cv2.COLOR_BGR2RGB))
+plt.title("flip Image") 
+#mirror
+plt.figure(figsize=(10,10))
+plt.subplot(1,2,1)
+plt.imshow(cv2.cvtColor(im,cv2.COLOR_BGR2RGB))
+plt.title("orignal")
+plt.subplot(1,2,2)
+plt.imshow(cv2.cvtColor(im_mirror,cv2.COLOR_BGR2RGB))
+plt.title("mirror Image") 
+
+plt.show()
+print("End of line....")
